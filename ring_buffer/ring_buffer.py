2af78d672c293817b795f6d51c1d25300d4141df
Aja Blanco <aja@Ajas-MacBook-Pro.local>
2020-07-17 11:05:42 -0700

    ring buffer complete, tests passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,24 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        # define oldest elements index
+        self.oldest_el_index = 0
+        # initialize ring_buffer
+        self.ring_buffer = []
 
     def append(self, item):
-        pass
-
+        # check if length of ring_buffer is less than capacity
+        if len(self.ring_buffer) < self.capacity:
+            # append item
+            self.ring_buffer.append(item)
+        # else
+        else:
+            # reassign oldest el to new item
+            self.ring_buffer[self.oldest_el_index] = item
+            # increment oldest el index
+            self.oldest_el_index += 1
+        # if oldest el is at capacity, reset it to 0
+        if self.oldest_el_index == self.capacity:
+            self.oldest_el_index = 0
     def get(self):
-        pass
+        return self.ring_buffer
